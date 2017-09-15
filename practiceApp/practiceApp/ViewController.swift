0c8a4e08a0a8c5cc6313392d7d3ae24d74555464
Korman Chen <kormanchen@kormans-mbp.attlocal.net>
2017-09-14 19:29:56 -0700

    added practiceApp folder

---
+++ practiceApp/practiceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practiceApp
+//
+//  Created by Korman Chen on 9/14/17.
+//  Copyright © 2017 Korman Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
