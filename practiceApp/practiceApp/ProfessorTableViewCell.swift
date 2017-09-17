3ddd9732c96b213b515b74d3a29521ba9ee7849d
Korman Chen <kormanchen@Kormans-MacBook-Pro.local>
2017-09-17 14:21:23 -0700

    Added TableViewController

---
+++ practiceApp/practiceApp/ProfessorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfessorTableViewCell.swift
+//  practiceApp
+//
+//  Created by Korman Chen on 9/16/17.
+//  Copyright © 2017 Korman Chen. All rights reserved.
+//
+
+import UIKit
+
+class ProfessorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
